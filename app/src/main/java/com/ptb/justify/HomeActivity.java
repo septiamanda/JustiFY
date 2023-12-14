5da608e7cb6ff8026221a310db8587d4a87b561f
windadelia24 <windadelia113@gmail.com>
2023-12-14 15:19:54 +0700

    yes

--- app/src/main/java/com/ptb/justify/HomeActivity.java
+++ app/src/main/java/com/ptb/justify/HomeActivity.java
@@ -97,7 +97,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(HomeActivity.this, "aaaaa", Toast.LENGTH_SHORT).show();
-                Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                Intent home = new Intent(getApplicationContext(), UU.class);
                 startActivity(home);
             }
         });
